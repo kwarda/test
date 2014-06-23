16fe0c2c294b9e0247661e3a466018881643ed57
kwarda <krzycho.warda@gmail.com>
2014-06-23 23:23:11 +0200

    fix-no-date

--- hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
+++ hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +14,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView)findViewById(R.id.message)).setText(new Date().toString());
     }
 }
