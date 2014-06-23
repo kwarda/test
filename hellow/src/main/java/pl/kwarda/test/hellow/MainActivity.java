fae55863994550b8980c61564be57598b527c6c4
kwarda <krzycho.warda@gmail.com>
2014-06-24 00:09:30 +0200

    login activity connect

--- hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
+++ hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
@@ -1,9 +1,11 @@
 package pl.kwarda.test.hellow;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -15,6 +17,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView)findViewById(R.id.details)).setText(new Date().toString());
+        findViewById(R.id.main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goNext(v);
+            }
+        });
+    }
 
+    public void goNext(View view) {
+        startActivity(new Intent(this,LoginActivity.class));
     }
 }
