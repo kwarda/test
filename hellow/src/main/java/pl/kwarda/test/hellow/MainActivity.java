0b9c380e2ee2a319ed47765e49b60d870048cc29
kwarda <krzycho.warda@gmail.com>
2014-06-23 23:41:34 +0200

    login activity

--- hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
+++ hellow/src/main/java/pl/kwarda/test/hellow/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView)findViewById(R.id.details)).setText(new Date().toString());
+
     }
 }
